53852efbf221053eb50b86cf0db72c00dec5361c
RahmaniLydia <145360815+RahmaniLydia@users.noreply.github.com>
2023-12-05 15:38:57 +0100

    modification DAO et BO info cadeaux

---
+++ BO/BO_info_cadeaux.php
@@ -0,0 +1,53 @@
+<?php
+
+
+  require_once($_SERVER['DOCUMENT_ROOT'] . '/Projet/DO/DO_info_cadeaux.php');
+  require_once($_SERVER['DOCUMENT_ROOT'] . '/Projet/DAO/DAO_info_cadeaux.php');
+  require_once($_SERVER['DOCUMENT_ROOT'] . '/Projet/DTO/DTO_info_cadeaux.php');
+
+	use infoCadeaux as infoCadeaux;
+	use infoCadeauxDAO as infoCadeauxDAO;
+	use infoCadeauxDTO as infoCadeauxDTO;
+
+
+class infoCadeauxBO {
+
+	// Service de récupératino de la liste des utilisateurs
+	function recupererListeUtilisateurs() {
+
+		/// Appel au DAO correspondant
+		$dao = new infoCadeauxDAO();
+		$listeDo = $dao->recupererListeUtilisateurs();
+
+		// Conversion de la liste des DO récupérés en DTO
+		$i = 0;
+		foreach ($listeDo as $do) {
+			$dto = new infoCadeauxDTO();
+			$dto->id_cadeau = $do->id_cadeau;
+			$dto->id_owner = $do->id_owner;
+			$dto->id_liste = $do->id_liste;
+
+      $dto->nom = $do->nom;
+			$dto->resume = $do->resume;
+			$dto->image = $do->image;
+
+      $dto->date_debut_reservation = $do->date_debut_reservation;
+			$dto->date_fin_reservation = $do->date_fin_reservation;
+			$dto->etat_reservation = $do->etat_reservation;
+
+
+			$infoCadeauxDTO[$i] = $dto;
+
+
+
+
+
+
+			$i++;
+		}
+
+		return $infoCadeauxDTO;
+	}
+}
+
+?>
---
+++ DAO/DAO_info_cadeaux.php
@@ -0,0 +1,63 @@
+<?php
+require_once($_SERVER['DOCUMENT_ROOT'] . '/Projet/DO/DO_info_cadeaux.php');
+
+
+class infoCadeauxDAO {
+
+	public $conn;
+
+	// Connexion à la base de données
+	function connectionBdd() {
+
+		// Déclaration des variables de connexion
+		$DB_HOST = "127.0.0.1";
+		$DB_NAME = "projetnoel";
+		$DB_PORT = 3306;
+		$DB_USER = "root";
+		$DB_PSWD = "root";
+
+		try {
+
+			$connString =
+				"mysql:host=".$DB_HOST.";dbname=".$DB_NAME.";
+				port=".$DB_PORT;
+				$this->conn = new PDO($connString, $DB_USER, $DB_PSWD);
+
+		}
+		catch(PDOException $e) {
+			die("Erreur : " . $e->getMessage());
+		}
+	}
+
+	// Requête de récupération des données en BDD
+	function recupererListeinfocadeaux() : array {
+
+		$this->connectionBdd();
+		$res = $this->conn->query("SELECT id_cadeau, id_owner,id_liste,nom,resume,prix,image, date_debut_reservation,date_fin_reservation,etat_reservation FROM info_cadeaux");
+
+
+		$i = 0;
+
+		foreach ($res as $row) {
+			$util = new infoCadeaux();
+			$util->id_cadeau = $row['id_cadeau'];
+			$util->id_owner = $row['id_owner'];
+			$util->id_liste = $row['id_liste'];
+      $util->nom = $row['nom'];
+      $util->resume = $row['resume'];
+      $util->prix = $row['prix'];
+      $util->image = $row['image'];
+      $util->nom = $row['nom'];
+      $util->date_debut_reservation = $row['date_debut_reservation'];
+      $util->date_fin_reservation = $row['date_fin_reservation'];
+      $util->etat_reservation = $row['etat_reservation'];
+			$infoCadeaux[$i] = $util;
+
+			$i++;
+		}
+
+		return $infoCadeaux;
+	}
+}
+
+?>
