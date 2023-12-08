c741117982ec8eae9362cc5052566f65bd35e498
RahmaniLydia <145360815+RahmaniLydia@users.noreply.github.com>
2023-12-08 09:28:55 +0100

    creation du DAO et BO de la table owner_reservation

---
+++ BO/BO_owner_reservation.php
@@ -0,0 +1,47 @@
+<?php
+
+
+  require_once($_SERVER['DOCUMENT_ROOT'] . '/Projet/DO/DO_owner_reservation.php');
+  require_once($_SERVER['DOCUMENT_ROOT'] . '/Projet/DAO/DAO_owner_reservation.php');
+  require_once($_SERVER['DOCUMENT_ROOT'] . '/Projet/DTO/DTO_owner_reservation.php');
+
+	use ownerReservation as ownerReservation;
+	use ownerReservationDAO as ownerReservationDAO;
+	use ownerReservationDTO as ownerReservationDTO;
+
+
+class ownerReservationBO {
+
+	// Service de récupératino de la liste des utilisateurs
+	function recupererListeownerreservation() {
+
+		/// Appel au DAO correspondant
+		$dao = new ownerReservationDAO();
+		$listeDo = $dao->recupererListeownerreservation();
+
+		// Conversion de la liste des DO récupérés en DTO
+		$i = 0;
+		foreach ($listeDo as $do) {
+			$dto = new ownerReservationDTO();
+			$dto->id_owner = $do->id_owner;
+			$dto->nom_owner = $do->nom_owner;
+			$dto->prenom_owner = $do->prenom_owner;
+
+
+
+
+			$ownerReservationDTO[$i] = $dto;
+
+
+
+
+
+
+			$i++;
+		}
+
+		return $OwnerReservationDTO;
+	}
+}
+
+?>
---
+++ DAO/DAO_owner_reservation.php
@@ -0,0 +1,56 @@
+<?php
+require_once($_SERVER['DOCUMENT_ROOT'] . '/Projet/DO/DO_owner_reservation.php');
+
+
+class ownerReservationDAO {
+
+	public $conn;
+
+	// Connexion à la base de données
+	function connectionBdd() {
+
+		// Déclaration des variables de connexion
+		$DB_HOST = "127.0.0.1";
+		$DB_NAME = "projetnoel";
+		$DB_PORT = 3306;
+		$DB_USER = "root";
+		$DB_PSWD = "root";
+
+		try {
+
+			$connString =
+				"mysql:host=".$DB_HOST.";dbname=".$DB_NAME.";
+				port=".$DB_PORT;
+				$this->conn = new PDO($connString, $DB_USER, $DB_PSWD);
+
+		}
+		catch(PDOException $e) {
+			die("Erreur : " . $e->getMessage());
+		}
+	}
+
+	// Requête de récupération des données en BDD
+	function recupererListeownerreservation() : array {
+
+		$this->connectionBdd();
+		$res = $this->conn->query("SELECT id_owner, nom_owner,prenom_owner FROM owner_reservation");
+
+
+		$i = 0;
+
+		foreach ($res as $row) {
+			$util = new ownerReservation();
+			$util->id_owner = $row['id_owner'];
+			$util->nom_owner = $row['nom_owner'];
+			$util->prenom_owner = $row['prenom_owner'];
+
+			$ownerReservation[$i] = $util;
+
+			$i++;
+		}
+
+		return $ownerReservation;
+	}
+}
+
+?>
