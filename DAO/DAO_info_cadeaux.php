bfa2803de9b224ab4ec1652dfb5af082a10748a2
Gaelle1363 <145127968+Gaelle1363@users.noreply.github.com>
2023-12-13 17:08:31 +0100

    Update DAO_info_cadeaux.php
    Ajout date_de_reservation

--- DAO/DAO_info_cadeaux.php
+++ DAO/DAO_info_cadeaux.php
@@ -1,6 +1,5 @@
 <?php
-//include('DO\DO_info_cadeaux.php');
-require_once($_SERVER['DOCUMENT_ROOT'] . '/Projet/DO/DO_info_cadeaux.php');
+include('DO\DO_info_cadeaux.php');
 use infoCadeauxDAO as infoCadeauxDAO;
 
 class infoCadeauxDAO {
@@ -12,7 +11,7 @@ class infoCadeauxDAO {
 
 		// Déclaration des variables de connexion
 		$DB_HOST = "localhost";
-		$DB_NAME = "projetnoel";
+		$DB_NAME = "ProjetNoel";
 		$DB_PORT = 3306;
 		$DB_USER = "root";
 		$DB_PSWD = "";
@@ -50,6 +49,7 @@ class infoCadeauxDAO {
       $util->image = $row['image'];
       $util->date_debut_reservation = $row['date_debut_reservation'];
       $util->date_fin_reservation = $row['date_fin_reservation'];
+			$util->date_de_reservation = $row['date_de_reservation'];
       $util->etat_reservation = $row['etat_reservation'];
 			$infoCadeaux[$i] = $util;
 
@@ -58,6 +58,97 @@ class infoCadeauxDAO {
 
 		return $infoCadeaux;
 	}
+
+	function supprimerListeinfocadeaux() : array {
+
+		$this->connectionBdd();
+
+
+
+		$query = "SELECT c.id_cadeau, c.id_owner, c.id_liste, c.nom, c.resume, c.prix, c.image, c.date_debut_reservation, c.date_fin_reservation, c.etat_reservation
+								FROM info_cadeaux c
+								INNER JOIN listes l ON c.id_liste = l.id_liste
+								WHERE l.id_auteur = :idAuteur";
+
+		$stmt = $this->conn->prepare($query);
+		$stmt->bindParam(':idAuteur', $idAuteur, PDO::PARAM_INT);
+		$stmt->execute();
+
+
+
+		$res = $this->conn->query("DELETE FROM info_cadeaux WHERE id_liste =0 ");
+
+
+
+
+
+		$res2 = $this->conn->query("SELECT id_cadeau, id_owner,id_liste,nom,resume,prix,image, date_debut_reservation,date_fin_reservation,date_de_reservation,etat_reservation FROM info_cadeaux");
+
+
+		$i = 0;
+
+		foreach ($res2 as $row) {
+			$util = new infoCadeaux;
+			$util->id_cadeau = $row['id_cadeau'];
+			$util->id_owner = $row['id_owner'];
+			$util->id_liste = $row['id_liste'];
+			$util->nom = $row['nom'];
+			$util->resume = $row['resume'];
+			$util->prix = $row['prix'];
+			$util->image = $row['image'];
+			$util->date_debut_reservation = $row['date_debut_reservation'];
+			$util->date_fin_reservation = $row['date_fin_reservation'];
+			$util->date_de_reservation = $row['date_de_reservation'];
+			$util->etat_reservation = $row['etat_reservation'];
+			$infoCadeaux2[$i] = $util;
+
+			$i++;
+		}
+
+		return $infoCadeaux2;
+
+	}
+
+function recupererListeinfocadeauxParAuteur($idAuteur) : array {
+			 // Connexion à la base de données
+			 $this->connectionBdd();
+
+			 // Requête de récupération des données en BDD
+			 $query = "SELECT c.id_cadeau, c.id_owner, c.id_liste, c.nom, c.resume, c.prix, c.image, c.date_debut_reservation, c.date_fin_reservation,c.date_de_reservation, c.etat_reservation
+									 FROM info_cadeaux c
+									 INNER JOIN listes l ON c.id_liste = l.id_liste
+									 WHERE l.id_auteur = :idAuteur";
+
+			 $stmt = $this->conn->prepare($query);
+			 $stmt->bindParam(':idAuteur', $idAuteur, PDO::PARAM_INT);
+			 $stmt->execute();
+
+
+			 $infoCadeaux = array(); // Initialisez votre tableau
+
+			 $i = 0;
+			 foreach ($stmt->fetchAll(PDO::FETCH_ASSOC) as $row) {
+					 $util = new infoCadeaux;
+					 $util->id_cadeau = $row['id_cadeau'];
+					 $util->id_owner = $row['id_owner'];
+					 $util->id_liste = $row['id_liste'];
+					 $util->nom = $row['nom'];
+					 $util->resume = $row['resume'];
+					 $util->prix = $row['prix'];
+					 $util->image = $row['image'];
+					 $util->date_debut_reservation = $row['date_debut_reservation'];
+					 $util->date_fin_reservation = $row['date_fin_reservation'];
+					 $util->date_de_reservation = $row['date_de_reservation'];
+					 $util->etat_reservation = $row['etat_reservation'];
+					 $infoCadeaux[$i] = $util;
+					 $i++;
+			 }
+
+			 return $infoCadeaux;
+	 }
+
+
 }
+
 
 ?>
