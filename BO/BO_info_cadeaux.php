ee258b12dd1d6ba273ea74c1f149f6e469ecc4be
Gaelle1363 <145127968+Gaelle1363@users.noreply.github.com>
2023-12-13 17:09:02 +0100

    Update BO_info_cadeaux.php
    Ajout date_de_reservation

--- BO/BO_info_cadeaux.php
+++ BO/BO_info_cadeaux.php
@@ -2,14 +2,8 @@
 
 
   //include('DO\DO_info_cadeaux.php');
-  //include('DAO\DAO_info_cadeaux.php');
-  //include('DTO\DTO_info_cadeaux.php');
-
-
-
-  require_once($_SERVER['DOCUMENT_ROOT'] . '/Projet/DO/DO_info_cadeaux.php');
-  require_once($_SERVER['DOCUMENT_ROOT'] . '/Projet/DAO/DAO_info_cadeaux.php');
-  require_once($_SERVER['DOCUMENT_ROOT'] . '/Projet/DTO/DTO_info_cadeaux.php');
+  include('DAO\DAO_info_cadeaux.php');
+  include('DTO\DTO_info_cadeaux.php');
 
 	use infoCadeaux as infoCadeaux;
 	use infoCadeauxDAO as infoCadeauxDAO;
@@ -17,44 +11,45 @@
 
 
 class infoCadeauxBO {
+  // Service de récupération de la liste des utilisateurs par auteur
+        function recupererListeinfocadeauxParAuteur($idAuteur) {
+            // Appel au DAO correspondant
+            $dao = new infoCadeauxDAO;
+            $listeDo = $dao->recupererListeinfocadeauxParAuteur($idAuteur);
+            $infoCadeauxDTO = array(); // Déclaration en dehors de la boucle
 
-	// Service de récupératino de la liste des utilisateurs
-	function recupererListeinfocadeaux() {
+            // Conversion de la liste des DO récupérés en DTO
+            $i = 0;
+            foreach ($listeDo as $do) {
+                $dto = new infoCadeauxDTO;
+                $dto->id_cadeau = $do->id_cadeau;
+                $dto->id_owner = $do->id_owner;
+                $dto->id_liste = $do->id_liste;
+                $dto->nom = $do->nom;
+                $dto->resume = $do->resume;
+                $dto->prix = $do->prix;
+                $dto->image = $do->image;
+                $dto->date_debut_reservation = $do->date_debut_reservation;
+                $dto->date_fin_reservation = $do->date_fin_reservation;
+                $dto->date_de_reservation = $do->date_de_reservation;
+                $dto->etat_reservation = $do->etat_reservation;
 
-		/// Appel au DAO correspondant
-		$dao = new infoCadeauxDAO;
-		$listeDo = $dao->recupererListeinfocadeaux();
+                $infoCadeauxDTO[$i] = $dto;
 
-		// Conversion de la liste des DO récupérés en DTO
-		$i = 0;
-		foreach ($listeDo as $do) {
-			$dto = new infoCadeauxDTO;
-			$dto->id_cadeau = $do->id_cadeau;
-			$dto->id_owner = $do->id_owner;
-			$dto->id_liste = $do->id_liste;
+                $i++;
+            }
 
-      $dto->nom = $do->nom;
-			$dto->resume = $do->resume;
-      $dto->prix = $do->prix;
-			$dto->image = $do->image;
-
-      $dto->date_debut_reservation = $do->date_debut_reservation;
-			$dto->date_fin_reservation = $do->date_fin_reservation;
-			$dto->etat_reservation = $do->etat_reservation;
-
-
-			$infoCadeauxDTO[$i] = $dto;
+            return $infoCadeauxDTO;
+        }
 
 
+function supprimerListeinfocadeaux(){
+  $dao = new infoCadeauxDAO;
+  $listeDo = $dao->supprimerListeinfocadeaux();
+  return $listeDo;
+}
 
 
-
-
-			$i++;
-		}
-
-		return $infoCadeauxDTO;
-	}
 }
 
 ?>
